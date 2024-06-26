27083ca681e59fe224d599c5b796e7423909baac
lurii <lurii_wly@163.com>
2024-06-27 02:20:48 +0800

    update

--- Code/QuantitativeAnalysis/RQ1/Get_Speed.py
+++ Code/QuantitativeAnalysis/RQ1/Get_Speed.py
@@ -8,20 +8,6 @@ import numpy as np
 import os
 import unicodedata
 
-def get_date(mes, community_name):
-    #community_name is the original dile name
-    mes = mes[mes.find(' <') + 2 : ].strip()             #add name to match the messgae -- get date
-    raw_file = open(community_name, "r",encoding= "utf-8")
-    for per_line in raw_file.readlines():
-        if mes in per_line:
-            weekday = per_line[1 : per_line.find("] ")]
-            break
-    #print(weekday)
-    weekday = weekday[weekday.find(" ") + 1 : ]
-    #print(weekday)
-    weeknum = datetime.datetime.strptime(weekday, "%H:%M:%S")
-    return weeknum
-
 #get csv of data
 root = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), "../../.."))  # get ccurrent dir
 data_df = pd.DataFrame({"DialogNum":[], "dialog_speed":[]})   
@@ -34,26 +20,42 @@ if not os.path.exists(res_dir):
 community_dirs = os.listdir(data_dir)
 for per_community in community_dirs:
     if ("unrespond" in per_community):
-        continue                             #only deal dilogs with responses
-    if ("android" not in per_community):
-        continue
+        continue    #只需要有回答的对话
+    
+    # if ('Typescript' not in per_community):
+    #     continue
     print(per_community)
     community_txt_dir = data_dir + per_community
     community_name = per_community.split(".")[0]
-    community_dir = root + "/Data/DataAscii/" + community_name + ".txt"   #Problem: Add process or not
-
+    
     dialog_count = 1
     data_df = pd.DataFrame({"DialogNum":[], "dialog_speed":[]})   
     with open (community_txt_dir, "r", encoding= "utf-8") as f:
-        first_utterance_flag = 1           #Tag the start of a dialog
-        cal_dialog_time = 0                #Tag whether the response time of a dialog has been calculated
+        first_utterance_flag = 1  #标记当前这句话是不是一个对话的开始
+        cal_dialog_time = 0 #标记这个对话是不是计算过时间了
         lines = f.readlines()
         for index, line in enumerate(lines):
             #print(line)
             if (first_utterance_flag == 1):
                 print(dialog_count)
+                #有可能开头的话是空的 因为都是ascii字符被删掉了
+                judge_space = line.replace(" ", "").replace(" ", "")  #判断消息是不是为空
+                if (judge_space.find('>') == len(judge_space) - 2):
+                    print("the dialog is empty start")
+                    first_utterance_flag = 0
+                    dialog_count += 1
+                    cal_dialog_time = 1
+                    continue
+                #判断名字是不是为'' 可能也是被字符处理导致的
+                judge_name = line[line.find(' <') + 2 : line.find('> ')]  #名字为空舍弃
+                if (judge_name == ""):
+                    first_utterance_flag = 0
+                    dialog_count += 1
+                    cal_dialog_time = 1
+                    print("the name is none")
+                    continue
                 first_name = line[line.find(' <') + 2 : line.find('> ')]
-                first_time = get_date(line, community_dir)
+                first_time = datetime.datetime.strptime(line[line.find('[') + 1 : line.find('] ')], "%Y-%m-%d %H:%M:%S")
                 #print(first_time)
                 res = []
                 res.append(dialog_count)
@@ -61,18 +63,19 @@ for per_community in community_dirs:
                 dialog_count += 1
                 continue
             if (line == "--------------------------------------------------------------------------------\n"):
-                first_utterance_flag = 1 #Tag the next utterance start a new dialog
+            #if (line == "--------------------------------------------------------------------------------------------------\n"):
+                first_utterance_flag = 1 #说明下一句开始是对话第一句
                 cal_dialog_time = 0
             #data_df.to_csv("D://zju//1-paper//1_newdataset//1-Discord_dataset//Rq1Analysis//rq1_res.csv")
             #break
             else:
-                # the utterance in a dialog but is not the initial one
+                #说明是一个对话中非第一句的其他句
                 if (cal_dialog_time == 0):
                     next_name = line[line.find(' <') + 2 : line.find('> ')]
                     if (next_name != first_name):
-                        next_time = get_date(line, community_dir)
+                        next_time = datetime.datetime.strptime(line[line.find('[') + 1 : line.find('] ')], "%Y-%m-%d %H:%M:%S")
                         #print(next_time)
-                        cal_dialog_time = 1    #The response time of this dialog has been calculated
+                        cal_dialog_time = 1  #该对话计算过时间
                         dialog_speed = next_time - first_time
                         res.append(dialog_speed.total_seconds())
                         #print(res)
